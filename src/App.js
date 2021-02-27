7e83c6ec047a8d1d85f8d565bcc8f07d28af00a1
camilo carvajal <camilocc597@gmail.com>
2021-02-26 20:13:22 -0500

    adding modal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Modal from "react-modal";
 
 function App() {
+  const [pet, setpet] = useState("");
+  const [pets, setpets] = useState([]);
+  const [editMode, seteditMode] = useState({ status: 1, action: "edit" });
+  const [modalupsert, setmodalupsert] = useState("");
+  //  const [modalIsOpen, setmodalIsOpen] = useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="modal">
+        <div className="modal-content">
+          <Modal isOpen={editMode.status}>
+            <div className="col-8">
+              <h2>
+                {editMode.action == "status"
+                  ? "Registrar"
+                  : "Actualiza tus datos"}
+              </h2>
+              <br></br>
+
+              <form>
+                <div>
+                  <h5>Mascota</h5>
+                  <hr></hr>
+                  <input
+                    type="text"
+                    className="form-control mb-2"
+                    placeholder="Nombre"
+                  ></input>
+                  <input
+                    type="text"
+                    className="form-control mb-2"
+                    placeholder="Tipo"
+                  ></input>
+                  <input
+                    type="text"
+                    className="form-control mb-2"
+                    placeholder="Raza"
+                  ></input>
+                  <input
+                    type="text"
+                    className="form-control mb-2"
+                    placeholder="Fecha"
+                  ></input>
+                </div>
+                <br></br>
+                <div>
+                  <h5>Dueno</h5>
+                  <hr></hr>
+                  <input
+                    type="text"
+                    className="form-control mb-2"
+                    placeholder="Nombre"
+                  ></input>
+                  <input
+                    type="text"
+                    className="form-control mb-2"
+                    placeholder="Telefono"
+                  ></input>
+                  <input
+                    type="text"
+                    className="form-control mb-2"
+                    placeholder="Direccion"
+                  ></input>
+                  <input
+                    type="text"
+                    className="form-control mb-2"
+                    placeholder="Email"
+                  ></input>
+                </div>
+              </form>
+              <button className="btn btn-primary">Save</button>
+            </div>
+          </Modal>
+        </div>
+      </div>
     </div>
   );
 }
